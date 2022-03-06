26099ca30694057375ef340c8e8e77e1d07eab57
noObject01 <emmanuel.ebhomenye@xpresspayments.com>
2022-03-06 02:34:54 +0100

    Added Width to list item.

--- lib/screens/home_screen/breaking_news_list_view.dart
+++ lib/screens/home_screen/breaking_news_list_view.dart
@@ -11,13 +11,12 @@ class BreakingNewsListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
     return Container(
         padding: EdgeInsets.only(left: 16, right: 16),
         child: ListView.separated(
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) {
-            return CardWidget(story: _story[index], screenSize: screenSize);
+            return CardWidget(story: _story[index]);
           },
           itemCount: _story.length,
           separatorBuilder: (BuildContext context, int index) {
@@ -30,52 +29,54 @@ class BreakingNewsListView extends StatelessWidget {
 class CardWidget extends StatelessWidget {
   final Story _story;
 
-  const CardWidget({
-    Key? key,
-    required Story story,
-    required this.screenSize,
-  })  : _story = story,
+  const CardWidget({Key? key, required Story story})
+      : _story = story,
         super(key: key);
-
-  final Size screenSize;
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
     return InkWell(
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          NewsIcon(
-            imageUrl: _story.imageUrl,
-            imageWidth: 250,
-          ),
-          SizedBox(height: 8),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('${_story.title}',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold)),
-              SizedBox(
-                height: 5,
-              ),
-              Text("4 hours ago",
-                  style: TextStyle(color: Colors.grey, fontSize: 14)),
-              SizedBox(
-                height: 5,
-              ),
-              Text("By David E. Sanger",
-                  style: TextStyle(color: Colors.grey, fontSize: 14)),
-              SizedBox(
-                height: 16,
-              ),
-            ],
-          )
-        ],
+      child: Container(
+        width: screenSize.width / 1.8,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            NewsIcon(
+              imageUrl: _story.imageUrl,
+              imageWidth: screenSize.width / 1.8,
+            ),
+            SizedBox(height: 8),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('${_story.title}',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 5,
+                ),
+                Text("4 hours ago",
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 5,
+                ),
+                Text("By ${_story.source}",
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold)),
+              ],
+            )
+          ],
+        ),
       ),
       onTap: () {
         Navigator.push(context, MaterialPageRoute(builder: (context) {
