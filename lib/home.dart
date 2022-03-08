968581522878f42c26528fb27b0b713f39519d99
noObject01 <emmanuel.ebhomenye@xpresspayments.com>
2022-03-08 02:19:42 +0100

    Created a new widget for switching theme.

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/provider/search_key_manager.dart';
 import 'package:news_app/screens/home_screen/home_screen.dart';
 import 'package:news_app/screens/search_screen/search_screen.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -20,23 +22,32 @@ class _HomeState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: <BottomNavigationBarItem>[
-          const BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          const BottomNavigationBarItem(
-              icon: Icon(Icons.search), label: 'Search'),
-          //const BottomNavigationBarItem( icon: Icon(Icons.account_circle), label: 'Profile'),
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => SearchKeyManager(),
+          ),
+          // ChangeNotifierProvider(
+          //   create: (context) => ThemeProvider(),
+          // ),
         ],
-      ),
-    );
+        child: Scaffold(
+          body: pages[_currentIndex],
+          bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            items: <BottomNavigationBarItem>[
+              const BottomNavigationBarItem(
+                  icon: Icon(Icons.home), label: 'Home'),
+              const BottomNavigationBarItem(
+                  icon: Icon(Icons.search), label: 'Search'),
+              //const BottomNavigationBarItem( icon: Icon(Icons.account_circle), label: 'Profile'),
+            ],
+          ),
+        ));
   }
 }
---
+++ lib/provider/theme_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeMode themeMode = ThemeMode.system;
+
+  bool get isDarkMode => themeMode == ThemeMode.dark;
+
+  void toggleTheme(bool isOn) {
+    themeMode = isOn ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+}
